e53610b2421809ad7178ed996578f18bfc7ec9a3
Angie <angieamado76@gmail.com>
2026-02-13 13:01:17 +0100

    feat: agrega prueba en app.tsx de rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
-import PreguntaMultiple from "./components/PreguntaMultiple"
-import PreguntaUnica from "./components/PreguntaUnica"
-import PreguntaVF from "./components/PreguntaVF"
-
-
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './components/auth/RegisterPage'
+import PreguntaMultiple from './components/PreguntaMultiple'
+import PreguntaUnica from './components/PreguntaUnica'
+import PreguntaVF from './components/PreguntaVF'
 
 function App() {
-  
-
   return (
-    <>
-    <PreguntaVF/>
-    <PreguntaUnica/>
-    <PreguntaMultiple/>
-    </>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Inicio</Link> | <Link to="/login">Login</Link> | <Link to="/register">Register</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={
+          <>
+            <h1>Inicio (componentes de prueba)</h1>
+            <PreguntaVF />
+            <PreguntaUnica />
+            <PreguntaMultiple />
+          </>
+        } />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
